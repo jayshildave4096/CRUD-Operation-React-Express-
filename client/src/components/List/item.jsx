c27b2005f55960bf8b11d99066ba111218d62f55
Jayshil Dave <davejayshil4096@gmail.com>
2020-06-25 10:12:20 +0530

    .

--- client/src/components/List/item.jsx
+++ client/src/components/List/item.jsx
@@ -76,13 +76,6 @@ class Item extends Component {
       cust_mail: this.state.cust_mail,
     };
     var p = [...this.state.posts];
-    console.log(data);
-    var idpost = p.find((post) => {
-      return post.id !== data.id;
-    });
-    console.log("idpost", idpost);
-    p = p.filter((post) => post !== idpost);
-    console.log("after", p);
     fetch("http://localhost:9000/users/remove", {
       method: "Delete",
       headers: {
@@ -99,6 +92,13 @@ class Item extends Component {
         return err;
       });
     console.log("before", p, data.id);
+
+    var idpost = p.find((post) => {
+      return post.id !== data.id;
+    });
+    console.log("idpost", idpost);
+    p = p.filter((post) => post !== idpost);
+    console.log("after", p);
 
     // this.props.reRender();
     this.setState({ isRemoved: true, posts: p });
